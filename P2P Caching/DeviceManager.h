43dd9d1f1c8c8060954bfbb81cabb8459167edd8
Alex <alex.krebiehl@gmail.com>
2014-01-30 17:07:10 -0500

    Initial commit

---
+++ P2P Caching/DeviceManager.h
@@ -0,0 +1,13 @@
+//
+//  DeviceManager.h
+//  P2P Caching
+//
+//  Created by Alex Krebiehl on 1/30/14.
+//  Copyright (c) 2014 NKU Research. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DeviceManager : NSObject
+
+@end
